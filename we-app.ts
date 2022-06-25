1aa0512cd12e24d11598abde804e78bfd3433bf4
m.ivanov <it.spec95@gmail.com>
2022-06-25 03:38:17 +0300

    commit

---
+++ we-app.ts
@@ -0,0 +1,15 @@
+class Message {
+    messageText: string;
+
+    constructor(text: string){
+        this.messageText = text;
+    }
+
+    showAlert(){
+        alert(this.messageText);
+    }
+}
+
+
+let test = new Message('Hello world');
+test.showAlert();
